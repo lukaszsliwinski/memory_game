cea09b207809dbd47bcdb95a66821f6ad6d111d3
lukaszsliwinski <lukaszsliwinski0@gmail.com>
2022-01-01 16:22:51 +0100

    Rebuild uncover function - add color comparison

--- script.js
+++ script.js
@@ -1,8 +1,43 @@
+let card1;
+let card2;
+let backgroundColor1;
+let backgroundColor2;
+let timeout;
+
+
+// Cover cards if their colors are not the same
+function coverAgain(card1, card2) {
+    timeout = setTimeout(
+        function(){
+            card1.classList.remove('uncovered');
+            card2.classList.remove('uncovered');
+        },500);
+};
+
+
+// Uncover card and compare colors
 function uncover(id) {
-    let card = document.getElementById(id)
-    if (card.classList.contains('uncovered')) {
-        console.log('uncovered');
+    if (!card1) {
+        // Uncover first card and get golor
+        card1 = document.getElementById(id);
+        card1.classList.add('uncovered');
+        backgroundColor1 = getComputedStyle(card1).getPropertyValue('background-color');
     } else {
-        card.classList.add('uncovered');
+        // Uncover second card and get color
+        if (id !== card1.id) {
+            card2 = document.getElementById(id);
+            card2.classList.add('uncovered');
+            backgroundColor2 = getComputedStyle(card2).getPropertyValue('background-color');
+        };
+
+        // Compare both colors
+        if (backgroundColor1 == backgroundColor2) {
+            } else {
+                coverAgain(card1, card2);
+            };
+
+        // Clear memory
+        card1 = undefined;
+        card2 = undefined;
     };
 };
